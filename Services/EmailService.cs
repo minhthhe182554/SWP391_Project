419466571a646c0967565c67646b4d1fcb972ce2
VuDuyTri <tri25012003@gmail.com>
2025-12-05 15:38:07 +0700

    abc

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -1,6 +1,28 @@
-﻿namespace SWP391_Project.Services
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace SWP391_Project.Services
 {
-    public class EmailService
+    public class EmailService : IEmailService
     {
+        private readonly string fromMail = "kakassj25@gmail.com";
+        private readonly string fromPassword = "mviantbwravxpyon";
+
+        public void SendMail(string toEmail, string subject, string body)
+        {
+            var message = new MailMessage();
+            message.From = new MailAddress(fromMail);
+            message.To.Add(new MailAddress(toEmail));
+            message.Subject = subject;
+            message.Body = body;
+            message.IsBodyHtml = true;
+
+            using (var smtpClient = new SmtpClient("smtp.gmail.com", 587))
+            {
+                smtpClient.Credentials = new NetworkCredential(fromMail, fromPassword);
+                smtpClient.EnableSsl = true;
+                smtpClient.Send(message);
+            }
+        }
     }
 }
